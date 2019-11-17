54e914aaf1aa8d43368ff41e1aff0fe0d765c96c
Matthew Mulholland <mmulholland@ets.org>
2019-11-16 23:09:47 -0500

    Remove tool package for installed packages for pylox

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 # Get version without importing, which avoids dependency issues
 exec(compile(open("pylox/version.py").read(),
@@ -9,12 +9,9 @@ setup(name="pylox",
       version=eval("__version__"),
       author="Matt Mulholland",
       author_email="mulhodm@gmail.com",
-      packages=["pylox", "pylox",
-                "tool", "tool"],
-      package_dir={"pylox": "pylox",
-                   "tool": "tool"},
+      packages=find_packages(exclude=["tool", "tests"]),
       include_package_data=True,
       entry_points={"console_scripts":
                         ["plox = pylox.pylox_interpreter:main",
-                         "generate_ast = tool.generate_ast:main"]},
+                         "pylox_generate_ast = tool.generate_ast:main"]},
       zip_safe=False)
